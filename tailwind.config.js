0fdb0395f5db87c560dd596d97d3a523d205de21
Antoine1998 <tonydahbar11@gmail.com>
2023-05-14 01:00:25 +1000

    styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.js",
     "./pages/**/*.js",
-    "./components/**/*.js",
+    "./Components/**/*.js",
  
     // Or if using `src` directory:
     // "./src/**/*.{js,ts,jsx,tsx}",
